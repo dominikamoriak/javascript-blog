ad7b934fe80ea29dc95e0ae875f7045857887d15
Dominika Moriak <dominika.moriak@gmail.com>
2023-01-10 15:38:40 +0100

    Add remove contents of titleList, const titleList - js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* [DONE] remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -15,12 +15,12 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* [DONE] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
 
-  /* [DONE] remove class 'active' from all articles */
+  /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts .active');
 
@@ -58,6 +58,8 @@ const optArticleSelector = '.post',
 function generateTitleLinks(){
 
   /* remove contents of titleList */
+
+const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
 
   /* for each article */
 
