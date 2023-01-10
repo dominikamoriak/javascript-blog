9bbd65678080176ad608e378d8eeb33b5d68d311
Dominika Moriak <dominika.moriak@gmail.com>
2023-01-10 22:12:11 +0100

    Create HTML of the link, const link HTML - js

--- js/script.js
+++ js/script.js
@@ -81,6 +81,9 @@ function generateTitleLinks(){
 
     /* create HTML of the link */
 
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
     /* insert link into titleList */
 
 }
