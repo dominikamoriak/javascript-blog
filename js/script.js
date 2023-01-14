66d3db8d505bd5c807d6137b144d199ab250e4c9
Dominika Moriak <dominika.moriak@gmail.com>
2023-01-14 16:41:21 +0100

    Add console.log to const tag - js

--- js/script.js
+++ js/script.js
@@ -149,8 +149,10 @@ function tagClickHandler(event){
 
   /* make a new constant "tag" and extract tag from the "href" constant */
   const tag = href.replace('#tag-', '');
+  console.log(tag);
 
   /* find all tag links with class active */
+
 
   /* START LOOP: for each active tag link */
 
