8ec11d58c7e096f2d8e1e482683a2dcdc0b11322
Dominika Moriak <dominika.moriak@gmail.com>
2023-01-14 14:22:42 +0100

    Add generated code to html variable - js

--- js/script.js
+++ js/script.js
@@ -119,6 +119,8 @@ function generateTags(){
     console.log(linkHTML);
 
     /* add generated code to html variable */
+    html = html + linkHTML;
+    console.log(html);
 
     /* END LOOP: for each tag */
   }
