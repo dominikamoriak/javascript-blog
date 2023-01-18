60d67b1cbf73217d6afee379640ebc14a498df2e
Dominika Moriak <dominika.moriak@gmail.com>
2023-01-18 20:00:41 +0100

    Fix errors - js

--- js/script.js
+++ js/script.js
@@ -92,6 +92,8 @@ function calculateTagsParams (){
   const params = {max:0, min:999999};
   console.log('params:', params);
 
+  const tags = document.querySelectorAll(optTagsListSelector);
+
   for(let tag in tags){
     console.log(tag + ' is used ' + tags[tag] + ' times');
     if(tags[tag] > params.max){
@@ -158,17 +160,22 @@ function generateTags(){
     /* END LOOP: for every article: */
   }
   /* [NEW] find list of tags in right column */
-  const tagList = document.querySelector(optTagsListSelector);
+  const tagList = document.querySelectorAll(optTagsListSelector);
 
   const tagsParams = calculateTagsParams(allTags);
   console.log('tagsParams:', tagsParams);
 
   /* [NEW] create variable for all links HTML code */
   let allTagsHTML = '';
+  console.log(allTagsHTML);
 
   /* [NEW] START LOOP: for each tag in allTags: */
   for(let tag in allTags){
+
     /* [NEW] generate code of a link and add it to allTagsHTML */
+    const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '('+ allTags[tag] + ')'+'</a></li>';
+    console.log(linkHTML);
+
     allTagsHTML += tag + '('+ allTags[tag] + ')';
     console.log(allTagsHTML);
   }
